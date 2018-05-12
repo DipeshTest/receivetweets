363565c39c80ccdf0ad4fb7f6c635986352c4e8e
dipesh <dipesh.response@gmail.com>
2018-05-12 15:54:01 +0530

    Trigger New Repo

---
+++ trigger.go
@@ -0,0 +1,144 @@
+package receivetweets
+
+import (
+	"context"
+	"errors"
+	"net/url"
+	"strings"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("trigger-twitter-listner")
+
+// TwitterTrigger is simple  trigger
+type TwitterTrigger struct {
+	metadata       *trigger.Metadata
+	config         *trigger.Config
+	handlers       []*trigger.Handler
+	topicToHandler map[string]*trigger.Handler
+}
+
+//NewFactory create a new Trigger factory
+func NewFactory(md *trigger.Metadata) trigger.Factory {
+	return &TwitterFactory{metadata: md}
+}
+
+// TwitterFactory  Trigger factory
+type TwitterFactory struct {
+	metadata *trigger.Metadata
+}
+
+//New Creates a new trigger instance for a given id
+func (t *TwitterFactory) New(config *trigger.Config) trigger.Trigger {
+	return &TwitterTrigger{metadata: t.metadata, config: config}
+}
+
+// Metadata implements trigger.Trigger.Metadata
+func (t *TwitterTrigger) Metadata() *trigger.Metadata {
+	return t.metadata
+}
+
+// Initialize implements trigger.Initializable.Initialize
+func (t *TwitterTrigger) Initialize(ctx trigger.InitContext) error {
+	t.handlers = ctx.GetHandlers()
+	return nil
+}
+
+// Start implements trigger.Trigger.Start
+func (t *TwitterTrigger) Start() error {
+
+	t.topicToHandler = make(map[string]*trigger.Handler)
+
+	apiKey := strings.TrimSpace(t.config.GetSetting("apiKey"))
+	apiSecret := strings.TrimSpace(t.config.GetSetting("apiSecret"))
+	accessToken := strings.TrimSpace(t.config.GetSetting("accessToken"))
+	accessTokenSecret := strings.TrimSpace(t.config.GetSetting("accessTokenSecret"))
+	stream := strings.TrimSpace(t.config.GetSetting("stream"))
+	searchString := strings.TrimSpace(t.handlers[0].GetStringSetting("searchString"))
+
+	//	log.Info("Twitter Started")
+
+	if len(apiKey) == 0 || len(apiSecret) == 0 || len(accessToken) == 0 || len(accessTokenSecret) == 0 || len(stream) == 0 || len(searchString) == 0 {
+		log.Info("Please check the input parameters")
+		panic("Please check the input parameters")
+		return errors.New("Please check the input parameters")
+	} else {
+
+		//		log.Info("Twitter Started")
+
+		for _, handler := range t.handlers {
+
+			topic := handler.GetStringSetting("searchString")
+			log.Info("Message to search", topic)
+			//	t.RunHandler(handler, "test1")
+
+			anaconda.SetConsumerKey(apiKey)
+			anaconda.SetConsumerSecret(apiSecret)
+			api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+			filter := url.Values{}
+			log.Info("Stream Started")
+			if stream == "user" {
+
+				filter.Set("follow", topic)
+
+			} else {
+				filter.Set("track", topic)
+			}
+
+			stream := api.PublicStreamFilter(filter)
+
+			defer stream.Stop()
+
+			for v := range stream.C {
+				twt, _ := v.(anaconda.Tweet)
+				log.Info("Received Tweet", twt.IdStr, twt.User.ScreenName, twt.Text)
+				t.RunHandler(handler, twt.IdStr, twt.User.ScreenName, twt.Text)
+			}
+
+			// ticker := time.NewTicker(60 * time.Second)
+			// go func() {
+			// 	for te := range ticker.C {
+			// 		s := time.Now().String()
+			// 		fmt.Println(te.Hour())
+			// 		t.RunHandler(handler, s)
+			// 	}
+
+			//	}()
+
+			log.Debugf("topic: [%s]", topic)
+
+		}
+
+	}
+
+	return nil
+}
+
+// Stop implements ext.Trigger.Stop
+func (t *TwitterTrigger) Stop() error {
+	//unsubscribe from topic
+
+	return nil
+}
+
+// RunHandler runs the handler and associated action
+func (t *TwitterTrigger) RunHandler(handler *trigger.Handler, id string, name string, message string) {
+
+	trgData := make(map[string]interface{})
+	trgData["tweetId"] = id
+	trgData["screenName"] = name
+	trgData["message"] = message
+
+	results, err := handler.Handle(context.Background(), trgData)
+
+	if err != nil {
+		log.Error("Error starting action: ", err.Error())
+	}
+	log.Debugf("Ran Handler: [%s]", results)
+	log.Debugf("Ran Handler: [%s]", handler)
+
+}
---
+++ trigger_test.go
@@ -0,0 +1,111 @@
+package mytrigger
+
+import (
+	"io/ioutil"
+	"testing"
+	//MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+var jsonTestMetadata = getTestJsonMetadata()
+
+func getTestJsonMetadata() string {
+	jsonMetadataBytes, err := ioutil.ReadFile("trigger.json")
+	if err != nil {
+		panic("No Json Metadata found for trigger.json path")
+	}
+	return string(jsonMetadataBytes)
+}
+
+const testConfig string = `{
+  "name": "flogo-mqtt",
+  "settings": {
+    "apikey": "flogo/#",
+    "consumerKey": "tcp://127.0.0.1:1883",
+    "consumerSecret": "flogoEngine",
+    "user": "",
+    "password": "",
+    "store": "",
+    "qos": "0",
+    "cleansess": "false"
+  },
+  "handlers": [
+    {
+      "actionId": "device_info",
+      "settings": {
+        "searchString": "test_start"
+      }
+    }
+  ]
+}`
+
+//type TestRunner struct {
+//}
+//
+//// Run implements action.Runner.Run
+//func (tr *TestRunner) Run(context context.Context, action action.Action, uri string, options interface{}) (code int, data interface{}, err error) {
+//	log.Debugf("Ran Action: %v", uri)
+//	return 0, nil, nil
+//}
+//
+//func (tr *TestRunner) RunHandler(ctx context.Context, act action.Action, options map[string]interface{}) (results map[string]*data.Attribute, err error) {
+//	log.Debugf("Ran Action: %v", act.Config().Id)
+//	return nil, nil
+//}
+
+func TestInit(t *testing.T) {
+
+	// New  factory
+	// md := trigger.NewMetadata(jsonTestMetadata)
+	// f := NewFactory(md)
+	//
+	// // New Trigger
+	// config := trigger.Config{}
+	// json.Unmarshal([]byte(testConfig), &config)
+	// fmt.Println(config)
+	// tgr := f.New(&config)
+	//
+	// tgr.Start()
+
+	//runner := &TestRunner{}
+
+	//tgr.Init(runner)
+}
+
+/*
+// TODO Fix this test
+func TestEndpoint(t *testing.T) {
+	// New  factory
+	md := trigger.NewMetadata(jsonMetadata)
+	f := NewFactory(md)
+	// New Trigger
+	config := trigger.Config{}
+	json.Unmarshal([]byte(testConfig), &config)
+	tgr := f.New(&config)
+	runner := &TestRunner{}
+	tgr.Init(runner)
+	tgr.Start()
+	defer tgr.Stop()
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("tcp://127.0.0.1:1883")
+	opts.SetClientID("flogo_test")
+	opts.SetUsername("")
+	opts.SetPassword("")
+	opts.SetCleanSession(false)
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	log.Debug("---- doing first publish ----")
+	token := client.Publish("test_start", 0, false, "Test message payload!")
+	token.Wait()
+	duration2 := time.Duration(2)*time.Second
+	time.Sleep(duration2)
+	log.Debug("---- doing second publish ----")
+	token = client.Publish("test_start", 0, false, "Test message payload!")
+	token.Wait()
+	duration5 := time.Duration(5)*time.Second
+	time.Sleep(duration5)
+	client.Disconnect(250)
+	log.Debug("Sample Publisher Disconnected")
+}
+*/
